dbae46fb3b11ecc9e4fddf7c0a78ea4e7f2abb85
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2023-12-07 23:10:11 +0530

    Add files via upload

--- 3.Linear_Data_Structures-Stacks/4.Undo_OR_Redo_Operations/code.js
+++ 3.Linear_Data_Structures-Stacks/4.Undo_OR_Redo_Operations/code.js
@@ -1,1 +1,72 @@
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
+class Stack {
+  constructor() {
+    this.top = new Node(null, null);
+    this.size = 0;
+  }
+
+  push(val) {
+    var inNode = new Node(val, this.top);
+    this.top = inNode;
+    this.size++;
+  }
+
+  peek() {
+    if (this.size > 0) {
+      return this.top.value;
+    } else {
+      throw "Stack Underflow";
+    }
+  }
+
+  pop() {
+    if (this.size > 0) {
+      var retVal = this.top.value;
+      this.top = this.top.next;
+      this.size--;
+      return retVal;
+    } else {
+      throw "Stack Underflow";
+    }
+  }
+
+  inEmpty() {
+    return this.size == 0;
+  }
+
+  print() {
+    var holder = this.top;
+    while (holder.next != null) {
+      console.log(holder.value);
+      holder = holder.next;
+    }
+  }
+}
+
+var undoStack = new Stack();
+var redoStack = new Stack();
+var counter = 0;
+
+function incrementCounter() {
+  undoStack.push(counter);
+  counter++;
+  document.getElementById("val").value = counter;
+}
+
+function undo() {
+  redoStack.push(counter);
+  counter = undoStack.pop();
+  document.getElementById("val").value = counter;
+}
+
+function redo() {
+  undoStack.push(counter);
+  counter = redoStack.pop();
+  document.getElementById("val").value = counter;
+}
