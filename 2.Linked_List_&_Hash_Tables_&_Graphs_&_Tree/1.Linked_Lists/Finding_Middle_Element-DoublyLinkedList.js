3411aeef2f4add9e240e21c585f8d824ce0e23a5
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2023-12-14 22:55:21 +0530

    Add files via upload

---
+++ 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/1.Linked_Lists/Finding_Middle_Element-DoublyLinkedList.js
@@ -0,0 +1,110 @@
+class Node {
+  constructor(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.front = new Node(null, null, null);
+    this.rear = new Node(null, null, null);
+    this.size = 0;
+  }
+
+  insertFront(val) {
+    var nodeIn = new Node(val, null, null);
+
+    if (this.size == 0) {
+      this.front = this.rear = nodeIn;
+    } else {
+      nodeIn.next = this.front;
+      this.front.prev = nodeIn;
+      this.front = nodeIn;
+    }
+    this.size++;
+  }
+
+  print() {
+    var holder = this.front;
+
+    while (holder != null) {
+      console.log(holder.value);
+      holder = holder.next;
+    }
+  }
+
+  insert(val, index) {
+    if (index > this.size) {
+      throw "IndexOutOfRange";
+    } else {
+      if (index == 0) {
+        this.insertFront(val);
+      } else {
+        var i = 0;
+        var holder = this.front;
+
+        var newNode = new Node(val, holder.next, holder);
+
+        if (holder.next != null) {
+          holder.next.prev = newNode;
+        }
+        holder.next = newNode;
+        this.size++;
+      }
+    }
+  }
+
+  deleteFront() {
+    this.front = this.front.next;
+    this.size--;
+  }
+
+  delete(index) {
+    if (index > this.size) {
+      throw "IndexOutOfRange";
+    } else {
+      if (this.size == 0) {
+        this.deleteFront();
+      } else {
+        var i = 0;
+        var holder = this.front;
+
+        while (i < index - 1) {
+          holder = holder.next;
+          i++;
+        }
+        holder.next.next.prev = holder;
+        holder.next = holder.next.next;
+      }
+    }
+  }
+
+  printReverse() {
+    var holder = this.rear;
+
+    while (holder != null) {
+      console.log(holder.value);
+      holder = holder.prev;
+    }
+  }
+
+  printMiddle() {
+    var slow = this.front;
+    var fast = this.front;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    console.log(slow.value);
+  }
+}
+
+var l = new LinkedList();
+
+l.insertFront(1);
+l.insertFront(2);
+l.insert(3, 1);
+l.printMiddle();
