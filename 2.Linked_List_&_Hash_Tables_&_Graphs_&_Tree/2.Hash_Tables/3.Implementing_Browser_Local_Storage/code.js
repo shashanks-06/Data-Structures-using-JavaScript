010ba040f6f850204d4fb4f3a0d40f97bf28d47c
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2023-12-15 16:29:11 +0530

    Add files via upload

--- 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/2.Hash_Tables/3.Implementing_Browser_Local_Storage/code.js
+++ 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/2.Hash_Tables/3.Implementing_Browser_Local_Storage/code.js
@@ -1,1 +1,32 @@
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.values = {};
+  }
 
+  calculateHash(value) {
+    var sum = 0;
+
+    for (var i = 0; i < value.length; i++) {
+      var charCode = value.charCodeAt(i);
+      sum += charCode;
+    }
+    return sum % this.size;
+  }
+
+  add(key, value) {
+    var hash = this.calculateHash(key);
+    this.values[hash] = value;
+  }
+
+  getItem(key) {
+    var hash = this.calculateHash(key);
+    return this.values[hash];
+  }
+}
+
+var h = new HashTable(51);
+
+h.add("Hello", "Worlds!");
+
+console.log(h.getItem("Hello"));
