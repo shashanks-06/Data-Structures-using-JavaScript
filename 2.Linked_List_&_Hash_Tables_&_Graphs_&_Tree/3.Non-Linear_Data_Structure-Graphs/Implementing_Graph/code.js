c87ad0d825117c776e9d549dbb9dc8bc5d2aa589
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2023-12-22 23:55:07 +0530

    Add files via upload

--- 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/3.Non-Linear_Data_Structure-Graphs/Implementing_Graph/code.js
+++ 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/3.Non-Linear_Data_Structure-Graphs/Implementing_Graph/code.js
@@ -1,1 +1,38 @@
+class Graph {
+  constructor() {
+    this.adjList = new Map();
+  }
 
+  addEdgeHelper(node, adj, weight) {
+    if (this.adjList.has(node)) {
+      var currSet = this.adjList.get(node);
+      currSet.add({ adj, weight });
+      this.adjList.set(node, currSet);
+    } else {
+      this.adjList.set(node, new Set([{ adj, weight }]));
+    }
+  }
+
+  addEdge(node, adj, weight) {
+    this.addEdgeHelper(node, adj, weight);
+  }
+
+  getEdges(node) {
+    return this.adjList.get(node);
+  }
+
+  getAllNodes() {
+    return Array.from(this.adjList.keys());
+  }
+
+  print() {
+    console.log(this.adjList);
+  }
+}
+
+var g = new Graph();
+g.addEdge(0, 1, 0);
+g.addEdge(0, 2, 0);
+g.addEdge(1, 2, 0);
+
+g.print();
