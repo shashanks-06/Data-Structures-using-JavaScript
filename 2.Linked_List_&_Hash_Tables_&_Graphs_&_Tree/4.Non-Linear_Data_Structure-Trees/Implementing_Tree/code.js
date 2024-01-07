a29278b4c139cea6881854740d9fc2659e2d28b6
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-01-07 23:08:44 +0530

    Add files via upload

--- 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/4.Non-Linear_Data_Structure-Trees/Implementing_Tree/code.js
+++ 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/4.Non-Linear_Data_Structure-Trees/Implementing_Tree/code.js
@@ -1,1 +1,58 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+class Bst {
+  constructor() {
+    this.root = null;
+  }
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left == null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right == null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  insert(data) {
+    var newNode = new Node(data);
+
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  inOrderRec(node) {
+    if (node != null) {
+      this.inOrderRec(node.left);
+      console.log(node.data);
+      this.inOrderRec(node.right);
+    }
+  }
+
+  inOrder() {
+    this.inOrderRec(this.root);
+  }
+}
+
+var t = new Bst();
+t.insert(10);
+t.insert(5);
+t.insert(15);
+
+t.inOrder();
