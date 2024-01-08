9c168b24290d55aed936f7b78b1f79a3e50b00a9
Shashank S. Surjekar <143866820+shashanks-06@users.noreply.github.com>
2024-01-08 22:49:36 +0530

    Add files via upload

--- 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/4.Non-Linear_Data_Structure-Trees/Tree_Traversals/code.js
+++ 2.Linked_List_&_Hash_Tables_&_Graphs_&_Tree/4.Non-Linear_Data_Structure-Trees/Tree_Traversals/code.js
@@ -1,1 +1,165 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+class Bst {
+  constructor() {
+    this.root = null;
+  }
+  //----------------------------PART ONE------------------------------------//
+
+  //----- INSERTION -----//
+
+  insertNode(node, newNode) {
+    if (newNode.data < node.data) {
+      if (node.left == null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right == null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  insert(data) {
+    var newNode = new Node(data);
+
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  // IN ORDER ( Left, Root, Right )
+
+  inOrderRec(node) {
+    if (node != null) {
+      this.inOrderRec(node.left);
+      console.log(node.data);
+      this.inOrderRec(node.right);
+    }
+  }
+
+  inOrder() {
+    this.inOrderRec(this.root);
+  }
+
+  //----------------------------PART THREE------------------------------------//
+
+  // PRE ORDER ( Root, Left, Right)
+  preOrderRec(node) {
+    if (node != null) {
+      console.log(node.data);
+      this.preOrderRec(node.left);
+      this.preOrderRec(node.right);
+    }
+  }
+
+  preOrder() {
+    this.preOrderRec(this.root);
+  }
+
+  // POST ORDER ( Left, Right, Root)
+
+  postOrderRec(node) {
+    if (node != null) {
+      this.postOrderRec(node.left);
+      this.postOrderRec(node.right);
+      console.log(node.data);
+    }
+  }
+
+  postOrder() {
+    this.postOrderRec(this.root);
+  }
+
+  //----------------------------PART TWO------------------------------------//
+
+  containsRec(node, data) {
+    if (node == null) {
+      return false;
+    } else if (data < node.data) {
+      return this.containsRec(node.left, data);
+    } else if (data > node.data) {
+      return this.containsRec(node.right, data);
+    } else {
+      return true;
+    }
+  }
+
+  contains(data) {
+    return this.containsRec(this.root, data);
+  }
+
+  findMinNode(node) {
+    if (node.left == null) {
+      return node;
+    } else {
+      return this.findMinNode(node.left);
+    }
+  }
+
+  //----- DELETION -----//
+
+  removeNode(node, key) {
+    if (node === null) {
+      return null;
+    } else if (key < node.data) {
+      node.left = this.removeNode(node.left, key);
+      return node;
+    } else if (key > node.data) {
+      node.right = this.removeNode(node.right, key);
+      return node;
+      //Once Iteration is finished, we should return back node that we ended up at.So, this allows to facilitate the actual deletion afterwords.
+    } else {
+      if (node.left == null && node.right == null) {
+        node = null;
+        return node;
+      }
+      if (node.left == null) {
+        node = node.right;
+        return node;
+      }
+      if (node.right == null) {
+        node = node.left;
+        return node;
+      }
+
+      var aux = this.findMinNode(node.right);
+      node.data = aux.data;
+      //To replace thing we're trying to delete with smallest possible value from R.H.S. . Thus, it will follow property on both sides.
+
+      node.right = this.removeNode(node.right, aux.data);
+      return node;
+      //we swapped the smallest node with the one we're deleting
+    }
+  }
+
+  remove(data) {
+    this.root = this.removeNode(this.root, data);
+  }
+}
+
+var t = new Bst();
+t.insert(10);
+t.insert(5);
+t.insert(15);
+console.log("------------IN ORDER ------------");
+t.inOrder();
+console.log("----------------------------------");
+console.log("------------PRE ORDER ------------");
+t.preOrder();
+console.log("-----------------------------------");
+console.log("------------POST ORDER ------------");
+t.postOrder();
+console.log("-----------------------------------");
